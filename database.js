5e5b6dd55677227d5f152cf5d6b65f769f97ecfe
Alexito <forthingsLL@hotmail.com>
2023-11-10 13:58:36 -0600

    Implement
    Implements product name validation, order editing and deletion functions, and order fetching and formatting function

--- database.js
+++ database.js
@@ -19,3 +19,54 @@ const getOrders = () => {
     tx.executeSql('select * from ORDERS',[], (_, { rows }) => console.log(JSON.stringify(rows)));
   });
 };
+
+// si el usuario intenta registrar un producto con un nombre ya existente
+
+/*
+const insertProduct = (product, quantity, price) => {
+  db.transaction(tx => {
+    tx.executeSql('select * from products where name = ?', [product], (_, { rows }) => {
+      if (rows.length > 0) {
+        console.log('El nombre del producto ya está en uso.');
+      } else {
+        tx.executeSql('insert into products (name, quantity, price) values (?, ?, ?)', [product, quantity, price]);
+      }
+    });
+  }, null);
+};
+*/
+
+// Para permitir que el usuario edite o elimine una orden de compra
+
+/* 
+
+// Función para editar una orden de compra
+const editOrder = (id, product, quantity, price) => {
+  db.transaction(tx => {
+    tx.executeSql('update orders set product = ?, quantity = ?, price = ? where id = ?', [product, quantity, price, id]);
+  }, null);
+};
+
+// Función para eliminar una orden de compra
+const deleteOrder = (id) => {
+  db.transaction(tx => {
+    tx.executeSql('delete from orders where id = ?', [id]);
+  }, null);
+};
+
+*/
+
+
+// Función para obtener todas las órdenes de compra y formatearlas
+
+/*
+const getAndFormatOrders = () => {
+  db.transaction(tx => {
+    tx.executeSql('select * from orders', [], (_, { rows }) =>
+      rows._array.forEach(order =>
+        console.log(`ID de Orden: ${order.id}\nProducto: ${order.product}\nCantidad: ${order.quantity}\nPrecio: ${order.price}\n`)
+      )
+    );
+  });
+};
+*/
